0ba1cf0719a5e625104f6526f719675094b379b1
Fazreil Amreen <fazreil.amreen.abdul@rhbgroup.com>
2019-08-27 10:22:59 +0800

    parameterized workflow component

---
+++ nomnoml-src/workflow.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import nom from 'nomnoml'
+
+class Workflow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.generateSvg = this.generateSvg.bind(this)
+  }
+
+  generateSvg(src) {
+   const svg = nom.renderSvg(src);
+
+   return  {
+     __html: svg
+   }
+  }
+
+  render() {
+    return (
+      <div dangerouslySetInnerHTML={this.generateSvg(this.props.src)} />
+    )
+  }
+}
+
+export default Workflow;
